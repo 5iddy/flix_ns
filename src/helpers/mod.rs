b531fb7178623af3d2b62b9a4c375af7df50bd11
5iddy <siddhuyeturi@gmail.com>
2022-08-11 14:56:52 +0530

    modifications to GH Actions

--- src/helpers/mod.rs
+++ src/helpers/mod.rs
@@ -4,4 +4,5 @@ pub use name::{sanitize_name, validate_name, verified_name_owner};
 pub mod coins;
 pub use coins::{assert_sent_sufficient_coin, assert_sent_sufficient_coins};
 
+#[cfg(test)]
 pub mod testing;
